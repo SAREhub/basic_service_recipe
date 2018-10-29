977e7a197ab3b7310952fa8f540ba10f37f2ab6e
awasiak <a.wasiak@sare.pl>
2018-10-29 11:55:51 +0100

    refactor bin/test/clean.sh

--- recipe_content/bin/test/clean.sh
+++ recipe_content/bin/test/clean.sh
@@ -5,13 +5,4 @@ set -a
 source ./bin/test/.env
 set +a
 
-set +e
-n=0
-until [ $n -ge 5 ]
-do
-    dockerutil::clean_all_with_label ${TESTENV_LABEL} && break
-    n=$[$n+1]
-    sleep 2
-done
-set -e
-
+dockerutil::clean_all_with_label ${TESTENV_LABEL}
